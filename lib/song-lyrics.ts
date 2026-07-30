ac0b71b7b7b03a4cb94e986260e633a73422b0c0
Evan Siever <evandsiever@gmail.com>
2026-07-31 03:32:59 +0700

    Switch background music back to Katy Perry

--- lib/song-lyrics.ts
+++ lib/song-lyrics.ts
@@ -9,8 +9,8 @@ export interface LyricWord {
 }
 
 export const song = {
-  videoId: "CGmdlQA_RZ4",
-  title: "ABBA - The Winner Takes It All | Epic Orchestra (2020)",
+  videoId: "Ahha3Cqe_fk",
+  title: "",
 }
 
 export const lyrics: LyricLine[] = []
